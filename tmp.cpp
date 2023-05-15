c048a244f2bb6e62a7522c6a4f9de3cfb83a40f0
Leezed <leezed525@qq.com>
2023-05-15 10:13:50 +0800

    配置gitignore

--- tmp.cpp
+++ tmp.cpp
@@ -1,4 +1,5 @@
 #include <windows.h>
+
 #include <algorithm>
 #include <bitset>
 #include <cmath>
@@ -15,6 +16,8 @@
 #include <stack>
 #include <string>
 #include <vector>
+
+#include "tmp1.cpp"
 #define ms(x, n) memset(x, n, sizeof(x));
 using namespace std;
 //*(int(*)[10])arr_name
@@ -25,68 +28,19 @@ inline int lowbit(int x) { return x & -x; }
 const int MAP_SIZE = 10;
 
 int main() {
-    // const int* p;
-    // int a = 10000;
-    // p = &a;
-    // cout << *p << endl;
-    // a = 1;
-    // cout << *p << endl;
-    // int b = 666;
-    // p = &b;
-    // cout << *p << endl;
+    const int* p;
+    int a = 10000;
+    p = &a;
+    int b = 10;
+    p = &b;
+    int num = 0;
+    int* const p1 = &num;
+    *p1 += 1;
+    cout << *p1 << endl;
+    Apple apple = Apple(10, 20);
+    cout << apple.getWeight() << endl;
+    cout << apple.getPrice() << endl;
 
-    // int j = 0, i = 0;
-    // while (j++ < 10) {
-    //     printf("这是第%d次输出------", j);
-    //     for (i = 0; i < 1000000000; i++)
-    //         ;
-    //     printf("\r");
-    // }
-
-    // HANDLE console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
-    // if (console_handle == INVALID_HANDLE_VALUE) {
-    //     std::cerr << "Failed to get console handle.\n";
-    //     return 1;
-    // }
-
-    // DWORD bytes_written;
-    // if (!WriteConsole(console_handle, "\x1b[A", 3, &bytes_written, nullptr)) {
-    //     std::cerr << "Failed to move cursor.\n";
-    //     return 1;
-    // }
-
-    // std::cout << "Hello";
-
-    HANDLE console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (console_handle == INVALID_HANDLE_VALUE) {
-        cerr << "Failed to get console handle.\n";
-        return 1;
-    }
-
-    // 初始化地图
-    char map[MAP_SIZE][MAP_SIZE];
-    for (int i = 0; i < MAP_SIZE; i++) {
-        for (int j = 0; j < MAP_SIZE; j++) {
-            map[i][j] = '#';
-        }
-    }
-
-    // 输出地图
-    for (int i = 0; i < MAP_SIZE; i++) {
-        for (int j = 0; j < MAP_SIZE; j++) {
-            cout << map[i][j];
-        }
-        cout << endl;
-    }
-
-    // 在地图上修改某个点
-    int x = 5, y = 5;
-    map[x][y] = 'X';
-    COORD cur = {y, x};
-    SetConsoleCursorPosition(console_handle, cur);
-    cout << map[x][y];
-
-    SetConsoleCursorPosition(console_handle, {10,10});
-    system("pause");
+    // system("pause");
     return 0;
 }
